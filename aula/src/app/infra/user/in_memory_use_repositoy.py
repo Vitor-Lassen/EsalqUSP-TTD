ca5a874bf50e43ef30cd1725a56ea99a7a057b51
Vitor Lassen <vitor@lassen.com.br>
2025-01-29 20:38:00 -0300

    feat: segundo bloco aula TTD II

---
+++ aula/src/app/infra/user/in_memory_use_repositoy.py
@@ -0,0 +1,22 @@
+from domain.user.user_entity import User
+from domain.user.user_repository_interface import UserRepositoryInterface
+from uuid import UUID
+
+class InMemoryRepository(UserRepositoryInterface):
+    def __init__(self):
+        self.users:list[User] = []
+
+    def save(self, user: User)-> None:
+        self.users.append(user)
+
+    def get_by_id(self, user_id: UUID) -> User:
+        for user in self.users:
+            if user.id == user_id:
+                return user
+        return None
+    
+    def update(self, user):
+        to_update_user = self.get_by_id(user.id)
+        if to_update_user:
+            self.users.remove(to_update_user)
+            self.users.append(user)
